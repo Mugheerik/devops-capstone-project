3b4cbc1f13263b10291c47ed4967c33c77f9e8ec
Mugheer khattak <105882374+Mugheerik@users.noreply.github.com>
2024-01-23 19:44:15 +0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -9,6 +9,7 @@ from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -47,7 +48,6 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-
     @app.route("/accounts", methods=["GET"])
     def list_accounts():
         """
@@ -62,7 +62,6 @@ def create_accounts():
         app.logger.info("Returning [%s] accounts", len(account_list))
         return jsonify(account_list), status.HTTP_200_OK
 
-
     @app.route("/accounts/<int:account_id>", methods=["GET"])
     def get_accounts(account_id):
         """
@@ -76,8 +75,7 @@ def create_accounts():
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
         return account.serialize(), status.HTTP_200_OK
-
-
+        
     @app.route("/accounts/<int:account_id>", methods=["PUT"])
     def update_accounts(account_id):
         """
@@ -94,7 +92,6 @@ def create_accounts():
         account.update()
 
         return account.serialize(), status.HTTP_200_OK
-
 
     @app.route("/accounts/<int:account_id>", methods=["DELETE"])
     def delete_accounts(account_id):
